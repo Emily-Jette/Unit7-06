8d4a977f320dc85de7e88198ba5513d14722057c
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-25 15:02:33 +0000

    changed number to age

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const number = 14
+const age = 14
 let guess = 0
 
-while (guess !== number) {
-  guess = prompt('Guess a number between 1 and 20')
-  if (guess < number) {
+while (guess !== age) {
+  guess = prompt('My age between 1 and 20')
+  if (guess < age) {
     alert('Your guess was too small, try again')
-  } else if (guess > number) {
+  } else if (guess > age) {
     alert('Your guess was too big, try again')
   } else {
     alert('You guessed correctly. Good Job!')
