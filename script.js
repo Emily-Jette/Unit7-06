0fb4b829a5d607c70ab94b6ce69f001a5c8404c2
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-25 14:28:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let number = 14
+let guess = 0
+
+while (guess != number) {
+  guess = prompt('Guess a number between 1 and 20') 
+  if (guess < number) {
+    alert('Your guess was too small, try again')
+  } else if (guess > number) {
+    alert('Your guess was too big, try again')
+  } else {
+    alert('You guessed correctly. Good Job!')
+  }
+}
